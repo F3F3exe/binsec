77ec28da9dbc3eb8a696da2e9f9099bef2e53f78
Fiona W <willif@student.ethz.ch>
2025-02-20 19:55:24 +0100

    Update run_opt_passes.sh

--- examples/crocs-muni-ct-examples/run_opt_passes.sh
+++ examples/crocs-muni-ct-examples/run_opt_passes.sh
@@ -203,7 +203,6 @@ else
         echo $CLANG $CFLAGS -$OPT_LEVEL -S -emit-llvm $SOURCE_FILE -o \$UNIQUE_BASE.ll &&
         
 
-        echo $CLANG -$OPT_LEVEL $CFLAGS \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.out $LIBS &&
         $CLANG $CFLAGS -$OPT_LEVEL -S -emit-llvm $SOURCE_FILE -o \$UNIQUE_BASE.ll &&
         eval ${OPT} -S {} \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll &&
         for PASS in {}; do
@@ -211,6 +210,8 @@ else
           echo eval ${OPT} -S -passes=\"\$PASS\" \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll
           eval ${OPT} -S -passes=\"\$PASS\" \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll
         done
+        echo $CLANG -$OPT_LEVEL $CFLAGS \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.out $LIBS &&
+
         $CLANG -$OPT_LEVEL $CFLAGS \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.out $LIBS &&
         
         binsec_output=\"\$(binsec -sse -sse-script checkct_\$BASE_NAME.cfg -sse-depth 1000000 -checkct \$UNIQUE_BASE.out -sse-timeout 10)\"
