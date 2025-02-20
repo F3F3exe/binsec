caec5256fb9f0d46714ce6fd59cc08c64a22bfd7
Fiona W <willif@student.ethz.ch>
2025-02-20 18:30:50 +0100

    Update run_opt_passes.sh

--- examples/crocs-muni-ct-examples/run_opt_passes.sh
+++ examples/crocs-muni-ct-examples/run_opt_passes.sh
@@ -201,7 +201,7 @@ else
         UNIQUE_BASE=${BASE_NAME}_{#} 
         echo "-------------" {}
         echo $CLANG $CFLAGS -$OPT_LEVEL -S -emit-llvm $SOURCE_FILE -o \$UNIQUE_BASE.ll &&
-        echo eval "${OPT} -S {} ${UNIQUE_BASE}.ll -o ${UNIQUE_BASE}.ll" &&
+        echo eval \$OPT -S {} \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll &&
         echo $CLANG -$OPT_LEVEL $CFLAGS \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.out $LIBS &&
         $CLANG $CFLAGS -$OPT_LEVEL -S -emit-llvm $SOURCE_FILE -o \$UNIQUE_BASE.ll &&
         eval \$OPT -S {} \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll &&
@@ -236,7 +236,7 @@ if grep -q "^starting from core" "$config_file"; then
         echo OPT {}
 
         $CLANG $CFLAGS -$OPT_LEVEL -S -emit-llvm $SOURCE_FILE -o \$UNIQUE_BASE.ll &&
-        eval $OPT -S {} \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll &&
+        eval \$OPT -S {} \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.ll &&
         $CLANG -$OPT_LEVEL $CFLAGS \$UNIQUE_BASE.ll -o \$UNIQUE_BASE.out $LIBS &&
         
         core_dump=\"core_\$UNIQUE_BASE.snapshot\"
