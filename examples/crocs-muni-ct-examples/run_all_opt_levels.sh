34c62a8c652b3aaffbd43455bc7389254a19b347
Fiona W <willif@student.ethz.ch>
2025-02-21 18:23:07 +0100

    Update run_all_opt_levels.sh

--- examples/crocs-muni-ct-examples/run_all_opt_levels.sh
+++ examples/crocs-muni-ct-examples/run_all_opt_levels.sh
@@ -5,18 +5,20 @@ OPT_LEVELS=(O1 O2 O3)
 
 # List of targets
 targets=(
-  01 02 03 04 05 07 08 09 10 
+  02 03 04 05 07 08 10 
 )
 clang_version=(
   clang-7.1 clang-19
 )
 
 # Iterate over all optimization levels and targets
-for OPT_LEVEL in "${OPT_LEVELS[@]}"; do
-    for TARGET in "${targets[@]}"; do
-        echo "Running run_opt_passes.sh with OPT_LEVEL=$OPT_LEVEL and TARGET=$TARGET"
-        ./run_opt_passes.sh "$OPT_LEVEL" "$TARGET"
-    done
+for CLANG in "${clang_version[@]}"; do
+  for OPT_LEVEL in "${OPT_LEVELS[@]}"; do
+      for TARGET in "${targets[@]}"; do
+          echo "Running run_opt_passes.sh with OPT_LEVEL=$OPT_LEVEL and TARGET=$TARGET and CLANG=$CLANG"
+          ./run_opt_passes.sh "$OPT_LEVEL" "$TARGET" "$CLANG"
+      done
+  done
 done
 
 echo "All optimization levels and targets tested!"
