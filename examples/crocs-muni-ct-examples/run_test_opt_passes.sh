200414b9a7aa522bbba767e64671378991f07c0c
Fiona W <willif@student.ethz.ch>
2025-02-23 21:00:19 +0100

    Update run_test_opt_passes.sh

--- examples/crocs-muni-ct-examples/run_test_opt_passes.sh
+++ examples/crocs-muni-ct-examples/run_test_opt_passes.sh
@@ -17,12 +17,13 @@ LOG_FILE="Results/llvm_optimization_results_$(date +%Y%m%d_%H%M%S).txt"
 # Iterate over all optimization levels and targets
 for TARGET in "${targets[@]}"; do
 
-  echo "${TARGET}" >>  ${LOG_FILE}
+  echo "Target: ${TARGET}" >>  ${LOG_FILE}
   
 
   for CLANG in "${clang_version[@]}"; do
-    echo "Clang version: $CLANG" > ${LOG_FILE}
+    echo "Clang version: $CLANG" >> ${LOG_FILE}
     for OPT_LEVEL in "${OPT_LEVELS[@]}"; do
+        echo "Optimization level: $OPT_LEVEL" >> ${LOG_FILE}
       
           echo "Running run_opt_passes.sh with OPT_LEVEL=$OPT_LEVEL and TARGET=$TARGET and CLANG=$CLANG"
 
@@ -31,21 +32,23 @@ for TARGET in "${targets[@]}"; do
 
           if grep -q "insecure" "$RESULTS_FILE1"; then
               echo "  CT vulnerability detected!"
-              echo "  CT vulnerability detected!" >>  ${LOG_FILE}
+              echo "  CT vulnerability detected: ${RESULTS_FILE1}" >>  ${LOG_FILE}
 
-              echo "    Analysing Clang frontend optimizations:" >>  ${LOG_FILE}
+              echo "    Analysing Clang frontend optimizations:"
 
               RESULTS_FILE2=$(./run_clang_opt.sh "$OPT_LEVEL" "$TARGET" "$CLANG" | grep -oP 'Results/clang_frontend_optimization_results_.*\.txt')
               if grep -q " secure" "$RESULTS_FILE2"; then
-                echo "    Clang frontend optimization caused CT violation!"
+                echo "    Clang frontend optimization caused CT violation:  ${RESULTS_FILE2}"  >>  ${LOG_FILE}
+                echo "    Clang frontend optimization caused CT violation:  ${RESULTS_FILE2}"
               fi
 
-              echo "    Analysing LLVM optimization passes:" >>  ${LOG_FILE}
+              echo "    Analysing LLVM optimization passes:" 
 
-              echo "    LLVM optimization passes cause CT violation!"
+              
               RESULTS_FILE3=$(./test_opt_passes.sh "$OPT_LEVEL" "$TARGET" "$CLANG" | grep -oP 'Results/opt_passes_.*\.txt')
               if grep -q "insecure" "$RESULTS_FILE3"; then
-                echo "      LLVM pass caused CT violation!"
+                echo "      LLVM pass caused CT violation:  ${RESULTS_FILE2}"  >>  ${LOG_FILE}
+                echo "      LLVM pass caused CT violation:  ${RESULTS_FILE2}"
               fi
             
           else
